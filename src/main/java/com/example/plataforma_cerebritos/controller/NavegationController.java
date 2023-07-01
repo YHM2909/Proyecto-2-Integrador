e06f68623fbe0827a1f3a63f6229494bca910346
silverblade34 <mpacheco.tacay@gmail.com>
2023-06-30 21:44:51 -0500

    se esta creando un layout del sidebar para las paginas

--- src/main/java/com/example/plataforma_cerebritos/controller/NavegationController.java
+++ src/main/java/com/example/plataforma_cerebritos/controller/NavegationController.java
@@ -1,17 +1,16 @@
 package com.example.plataforma_cerebritos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class NavegationController {
-    @GetMapping
-    public String index(){
-        return "index";
-    }
-
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("tstamp", LocalDateTime.now());
+        return "dashboard";
     }
 }
