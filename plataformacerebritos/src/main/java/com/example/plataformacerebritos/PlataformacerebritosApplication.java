5482ad36c242998cc6afaa36b9ddc7dc5b79ea05
YHM2909 <yensuhme299@gmail.com>
2023-07-04 14:10:15 -0500

    cambios guardados

---
+++ plataformacerebritos/src/main/java/com/example/plataformacerebritos/PlataformacerebritosApplication.java
@@ -0,0 +1,13 @@
+package com.example.plataformacerebritos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformacerebritosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformacerebritosApplication.class, args);
+	}
+
+}
---
+++ plataformacerebritos/src/test/java/com/example/plataformacerebritos/PlataformacerebritosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plataformacerebritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformacerebritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
