b29159c42fb9d914562fd06107aee5a8975345bb
YHM2909 <137650244+YHM2909@users.noreply.github.com>
2023-07-07 05:47:11 -0500

    Cambios Guardados

---
+++ plataformacerebritos/src/main/java/com/example/plataforma_cerebritos/PlataformaCerebritosApplication.java
@@ -0,0 +1,13 @@
+package com.example.plataforma_cerebritos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaCerebritosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaCerebritosApplication.class, args);
+	}
+
+}
