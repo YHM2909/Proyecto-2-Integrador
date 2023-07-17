937a023911c4c6d641dfbf6e70977665dbdd6a37
Jean Mendoza <smilu.u@hotmail.com>
2023-07-16 23:34:08 -0500

    vistas e img

--- src/main/java/com/example/plataforma_cerebritos/controller/NavegationController.java
+++ src/main/java/com/example/plataforma_cerebritos/controller/NavegationController.java
@@ -1,17 +1,26 @@
 package com.example.plataforma_cerebritos.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class NavegationController {
-    @GetMapping
-    public String index(){
-        return "index";
-    }
 
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+@RestController
+public class NavegationController{
+
+    @Autowired
+    private TipoService tipoService;
+
+
+    @GetMapping("/excel")
+    public void generarExcel (HttpServletResponse response) throws  Exception{
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+        String headerValue= "attachment; filename=tipos.xlsx";
+        response.setHeader(headerKey,headerValue);
+        tipoService.generarExcel(response);
+
     }
 }
